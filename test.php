37f4ffd1ab896b0e9c605445879a577cad1200d8
wei.qin <wei.qin@91sports.cc>
2019-12-19 13:37:26 +0800

    test脚本

--- test.php
+++ test.php
@@ -1,2 +1,14 @@
 <?php
 
+require 'vendor/autoload.php';
+
+use Nesk\Puphpeteer\Puppeteer;
+
+$puppeteer = new Puppeteer;
+$browser = $puppeteer->launch();
+
+$page = $browser->newPage();
+$page->goto('https://example.com');
+$page->screenshot(['path' => 'example.png']);
+
+$browser->close();
