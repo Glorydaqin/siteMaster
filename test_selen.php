0809b9e0ba3b0be517c7715f47dbdb25a861d9ab
wei.qin <wei.qin@91sports.cc>
2020-01-16 18:05:14 +0800

    可用第一版。测试selenium 代码

--- test_selen.php
+++ test_selen.php
@@ -1,14 +1,21 @@
 <?php
+
 namespace Facebook\WebDriver;
 
 require 'vendor/autoload.php';
 
+set_time_limit(0);
 
 use Facebook\WebDriver\Chrome\ChromeOptions;
+use Facebook\WebDriver\Interactions\Internal\WebDriverCoordinates;
 use Facebook\WebDriver\Remote\DesiredCapabilities;
 use Facebook\WebDriver\Remote\RemoteWebDriver;
 
-try{
+$user = "692860800@qq.com";
+$password = "daqing";
+
+try {
+
 
     $host = 'http://192.168.136.121:4444/wd/hub/'; // this is the default
 //    $host = 'http://192.168.10.138:4444/wd/hub/'; // this is the default
@@ -17,26 +24,48 @@ try{
     $options = new ChromeOptions();
 //    $useragent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36';
 //    $options->addArguments(["user-agent={$useragent}"]);
-    $options->addArguments(["--incognito"]);
+//    $options->addArguments(["--incognito"]);
     $options->addArguments(["--lang=en"]);
-    $options->addArguments(["--disable-features=NetworkService"]);
+//    $options->addArguments(["--disable-features=NetworkService"]);
     $options->addArguments(["--window-size=1920,1080"]);
-    $options->addArguments(["headless"]);
+//    $options->addArguments(["headless"]);
 
     $capabilities->setCapability(ChromeOptions::CAPABILITY, $options);
 
     $driver = RemoteWebDriver::create($host, $capabilities, 5000);
 
     $driver->get('https://www.semrush.com/users/login.html');
+    echo "get login finish" . PHP_EOL;
 //    $driver->get('https://www.semrush.com');
 //    $driver->get('https://www.baidu.com');
-    $cookie = $driver->manage()->getCookies();
-    d($cookie);
-    d($driver->getPageSource());
+//    $cookie = $driver->manage()->getCookies();
+    $driver->manage()->timeouts()->implicitlyWait(15);    //隐性设置15秒
+    $driver->findElement(WebDriverBy::name('email'))->sendKeys($user);
+    $driver->findElement(WebDriverBy::name('password'))->sendKeys($password);
+    $driver->findElement(WebDriverBy::className('sc-btn__inner'))->click();
+    echo "click login button finish" . PHP_EOL;
+
+    //由于下拉框是通过点击“搜索设置”按钮触发JS动态生成的DOM，所以这里使用Wait for new element to appear方式，不然直接调用查找元素会报错，说找不到元素
+    $driver->wait(20)->until(
+        WebDriverExpectedCondition::urlContains("/dashboard")
+//        WebDriverExpectedCondition::visibilityOfElementLocated(
+//            WebDriverBy::className('srf-line')
+//        )
+    );
+
+    echo "login success" . PHP_EOL;
+    $html = $driver->getPageSource();
+    echo 'html strlen: ' . strlen($html) . PHP_EOL;
+
+    if (stripos($html, 'Please confirm your email')) {
+        echo "find Please confirm your email" . PHP_EOL;
+    }
+
+//    var_dump($driver->manage()->getCookies());
     $driver->quit();
 
-}catch (\Exception $exception){
+} catch (\Exception $exception) {
 
-    dd($exception);
+    var_dump($exception);
 }
 
