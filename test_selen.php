d427d5d9ff17aae3a615cb01959bc509936957c0
wei.qin <wei.qin@91sports.cc>
2020-01-16 11:31:35 +0800

    测试selenium 代码

--- test_selen.php
+++ test_selen.php
@@ -7,22 +7,22 @@ require 'vendor/autoload.php';
 use Facebook\WebDriver\Remote\DesiredCapabilities;
 use Facebook\WebDriver\Remote\RemoteWebDriver;
 
-$host = '192.168.2.103:4444/wd/hub'; // this is the default
-$capabilities = DesiredCapabilities::chrome();
-$driver = RemoteWebDriver::create($host, $capabilities, 5000);
-
 try{
 
-    $driver->get('https://www.google.com');
+    $host = 'http://127.0.0.1:4444/wd/hub/'; // this is the default
+//    $host = 'http://hub:4444/wd/hub/'; // this is the default
+    $capabilities = DesiredCapabilities::chrome();
+    $driver = RemoteWebDriver::create($host, $capabilities, 5000);
+
+    $driver->get('https://www.semrush.com/users/login.html');
+//    $driver->get('https://www.google.com');
     $cookie = $driver->manage()->getCookies();
     d($cookie);
     d($driver->getPageSource());
     $driver->quit();
 
-
 }catch (\Exception $exception){
 
-    $driver->quit();
     dd($exception);
 }
 
