b70431662fa3f053c539f9fceab56256c9658e47
wei.qin <wei.qin@91sports.cc>
2020-02-21 13:49:26 +0800

    插件初始版本

--- front/tpl/dashboard.php
+++ front/tpl/dashboard.php
@@ -73,9 +73,20 @@
                                 <?if($site['is_available']){?>
                                     <?if($site['name']=='mangools'){?>
                                         <div class="faq-item">
-                                            <a target="_blank" href="/plugins/vip-login.zip">插件下载地址</a>
-                                            <br>
-                                            <a target="_blank" href="/plugins/插件安装.gif">插件安装说明</a>
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <a href="/plugins/vip-login.zip" target="_blank" class="faq-question" style="color: #f8ac59">
+                                                        插件下载地址
+                                                    </a>
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <a href="/plugins/插件安装.gif" target="_blank" class="faq-question" style="color: #f8ac59">
+                                                        插件安装说明
+                                                    </a>
+                                                </div>
+                                            </div>
                                         </div>
                                     <?}else{?>
                                         <div class="faq-item">
