a78e71ea5d34bb47bc506020b913bc1cc72a7272
wei.qin <wei.qin@91sports.cc>
2020-04-17 17:49:42 +0800

    ahrefs接口

--- front/action/api_re_cookie.php
+++ front/action/api_re_cookie.php
@@ -15,16 +15,16 @@ $site_account_id = addslashes($site_account_id);
 
 $info = Account::get_site_account($site_account_id);
 if ($info) {
+
     $transfer = new Ahrefs($info['username'], $info['password']);
     $cookie_file = DIR_TMP_COOKIE . $transfer->cookie_key . ".txt";
     $result = $transfer->login();
     if ($result) {
         //取cookie内容
         $cookie_content = file_get_contents($cookie_file);
+        preg_match_all("/BSSESSID\t([^\s\n]+)/", $cookie_content, $match);
 
-        preg_match_all("/BSSESSID\t(.*?)\n/", $cookie_content, $match);
-
-        if (isset($match[0][1])) {
+        if (isset($match[1][0])) {
             //内容写db
             $sql = "update site_account set cookie = '{$match[1][0]}' where id = {$site_account_id}";
             $GLOBALS['db']->query($sql);
