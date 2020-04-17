a226a9c86a2d085aa1937d84c8ee852f3180c2c3
wei.qin <wei.qin@91sports.cc>
2020-04-17 21:40:00 +0800

    ahrefs 插件

---
+++ chrome-plugin-product/ahrefs/js/account.js
@@ -0,0 +1,13 @@
+chrome.webRequest.onBeforeSendHeaders.addListener(function (details) {
+      let ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36'
+      for (let i = 0; i < details.requestHeaders.length; ++i) {
+        if (details.requestHeaders[i].name === 'User-Agent') {
+          details.requestHeaders[i].value = ua;
+          break;
+        }
+      }
+      return {requestHeaders: details.requestHeaders};
+    },
+    {urls: ["<all_urls>"]},
+    ["blocking", "requestHeaders"]
+);
