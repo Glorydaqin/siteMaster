09b7db8fc865eafa66b29c381d561d38f6a83206
qinwei <692860800@qq.com>
2021-02-20 16:01:56 +0800

    save

--- electronApp/main/js/updater.js
+++ electronApp/main/js/updater.js
@@ -13,7 +13,6 @@ var downloadInfo = {
     downloadBytes: 0,
     totalBytes: 1,
 };
-var percent = 0;
 
 $.ajax({
     type: "GET",//使用get请求请求正确
@@ -56,40 +55,6 @@ function openUpgradeInfo() {
     });
 }
 
-// function downloadFile(file_url, targetPath) {
-//     // Save variable to know progress
-//     var received_bytes = 0;
-//     var total_bytes = 0;
-//
-//     var req = request({
-//         method: 'GET', uri: file_url
-//     });
-//
-//     var out = fs.createWriteStream(targetPath);
-//     req.pipe(out);
-//
-//     req.on('response', function (data) {
-//         // Change the total bytes value to get progress later.
-//         total_bytes = parseInt(data.headers['content-length']);
-//     });
-//
-//     req.on('data', function (chunk) {
-//         // Update the received bytes
-//         received_bytes += chunk.length;
-//
-//         showProgress(received_bytes, total_bytes);
-//     });
-//
-//     req.on('end', function () {
-//         alert("File succesfully downloaded");
-//     });
-// }
-//
-// function showProgress(received, total) {
-//     var percentage = (received * 100) / total;
-//     console.log(percentage + "% | " + received + " bytes out of " + total + " bytes.");
-// }
-
 var download = function (url, dest, cb) {
     var file = fs.createWriteStream(dest);
     var request = https.get(url, function (response) {
@@ -97,80 +62,104 @@ var download = function (url, dest, cb) {
 
         response.pipe(file);
         file.on('finish', function () {
-            file.close(cb);  // close() is async, call cb after close completes.
+            file.close(cb(true));  // close() is async, call cb after close completes.
         });
 
-        let rawData = '';
+        downloadInfo.downloadBytes = 0;
         response.on('data', (chunk) => {
-            rawData += chunk;
-
-            downloadInfo.downloadBytes = rawData.length;
+            downloadInfo.downloadBytes += chunk.length
             console.log('total percent:' + downloadInfo.downloadBytes / downloadInfo.totalBytes)
             $(".upgrade-box .process").width(Math.floor(downloadInfo.downloadBytes / downloadInfo.totalBytes * 100).toFixed(2) + '%')
         });
-        response.on('end', () => {
-            percent = 1;
-            downloadInfo.downloadBytes = downloadInfo.totalBytes;
-            console.log('total percent:1', dest)
-            $(".upgrade-box .process").width('100%')
-        });
+        // response.on('end', () => {
+        //     downloadInfo.downloadBytes = downloadInfo.totalBytes;
+        //     console.log('total percent:1', dest)
+        //     $(".upgrade-box .process").width('100%')
+        // });
 
     }).on('error', function (err) { // Handle errors
-        fs.unlink(dest); // Delete the file async. (But we don't check the result)
-        if (cb) cb(err.message);
+        fs.unlink(dest, function () {
+        }); // Delete the file async. (But we don't check the result)
+        if (cb) {
+            cb(false, err.message);
+        }
     });
 };
 
 $('#upgrade-btn').click(function () {
+
     if (isDownload) {
         return;
     }
     isDownload = true;
+    $("#upgrade-btn").addClass('disabled');
 
     var fileUrl = (os.platform() === 'darwin') ? latestVersion['file-mac'] : latestVersion['file-win'];
     var tmpFile = fileUrl.split("/")[fileUrl.split("/").length - 1];
     var savePath = os.tmpdir() + '/' + Math.random() + tmpFile;
-    console.log(fileUrl);
     console.log(savePath);
 
-    // var url = 'https://vtool.club/app/vip-login.exe';
     console.info(appName, appVersion);
 
-    download(fileUrl, savePath, function (result) {
+    download(fileUrl, savePath, function (result, msg) {
+        console.log(result, msg)
 
-        //下载完成或者异常
-        setTimeout(function () {
-            if (os.platform() === 'darwin') {
-                console.info(appName, appVersion);
-                // 挂载
-                cp.execSync(`hdiutil attach '${savePath}' -nobrowse`, {
-                    stdio: ['ignore', 'ignore', 'ignore']
-                });
+        if (!result) {
+            isDownload = false;
+            downloadInfo = {
+                percent: 0,
+                downloadBytes: 0,
+                totalBytes: 1,
+            };
+            $(".upgrade-box .process").width('0')
+            $("#upgrade-btn").removeClass('disabled');
 
-                // 覆盖原 app
-                cp.execSync(`rm -rf '/Applications/${appName}.app' && cp -R '/Volumes/${appName} ${appVersion}/${appName}.app' '/Applications/${appName}.app'`);
+            parent.layer.alert("更新下载失败,请检查网络", {
+                skin: 'layui-layer-molv', //样式类名
+                // anim: 5,
+                // closeBtn:1,
+                // title:'信息',
+            });
+        } else if (result && fs.existsSync(savePath)) {
+            //下载完成或者异常
+            setTimeout(function () {
+                if (os.platform() === 'darwin') {
+                    console.info(appName, appVersion);
+                    // 挂载
+                    cp.execSync(`hdiutil attach '${savePath}' -nobrowse`, {
+                        stdio: ['ignore', 'ignore', 'ignore']
+                    });
 
-                // 卸载挂载的 dmg
-                cp.execSync(`hdiutil eject '/Volumes/${appName} ${appVersion}'`, {
-                    stdio: ['ignore', 'ignore', 'ignore']
-                });
+                    // 覆盖原 app
+                    cp.execSync(`rm -rf '/Applications/${appName}.app' && cp -R '/Volumes/${appName} ${appVersion}/${appName}.app' '/Applications/${appName}.app'`);
 
-                // 重启
-                let result = ipcRenderer.sendSync('appAction', 'relaunch');
-                result = ipcRenderer.sendSync('appAction', 'quit');
-            }
+                    // 卸载挂载的 dmg
+                    cp.execSync(`hdiutil eject '/Volumes/${appName} ${appVersion}'`, {
+                        stdio: ['ignore', 'ignore', 'ignore']
+                    });
 
-            if (os.platform() === 'win32') {
-                shell.openItem(savePath);
-                setTimeout(function () {
-                    result = ipcRenderer.sendSync('appAction', 'quit');
-                }, 1500)
-            }
-        }, 2000)
+                    // 重启
+                    ipcRenderer.sendSync('appAction', 'relaunch');
+                    ipcRenderer.sendSync('appAction', 'quit');
+                }
 
-    });
+                if (os.platform() === 'win32') {
+                    shell.openItem(savePath);
+                    setTimeout(function () {
+                        ipcRenderer.sendSync('appAction', 'quit');
+                    }, 1500)
+                }
+            }, 2000)
+        }
+
+    })
 
 })
+
+
+function doInstall() {
+
+}
 
 function upgradeCancel() {
     // $('.upgrade-box').hide();
